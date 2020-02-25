0805c05d0f25a36f88d3cb1a8ca949b6f64cd2de
jayatejareddy <49113002+jayatejareddy@users.noreply.github.com>
2020-02-25 19:19:45 +0530

    Add files via upload

---
+++ Problem6/problem6.py
@@ -0,0 +1,8 @@
+s1=0
+s2=0
+for i in range(1,101):
+    s1+=i
+for i in range(1,101):
+    s2+=i**2
+print(s1**2-s2,s1,s2)   
+"	s = sum(i for i in range(1, N + 1)) alternatively this also used for simlicity"
